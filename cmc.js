09e7d394dd78897b0c84b6e5127b42d87943b0dc
Gokul P R <gokulpr47@gmail.com>
2021-11-05 23:55:52 +0530

    setting up command to display bitcoin value in inr

---
+++ cmc.js
@@ -0,0 +1,23 @@
+const { cmc_token } = require('./config.json');
+
+const rp = require('request-promise');
+const requestOptions = {
+  method: 'GET',
+  uri: 'https://sandbox-api.coinmarketcap.com/v1/cryptocurrency/listings/latest',
+  qs: {
+    'start': '1',
+    'limit': '5000',
+    'convert': 'USD'
+  },
+  headers: {
+    'X-CMC_PRO_API_KEY': cmcToken,
+  },
+  json: true,
+  gzip: true
+};
+
+rp(requestOptions).then(response => {
+  console.log('API call response:', response);
+}).catch((err) => {
+  console.log('API call error:', err.message);
+});
